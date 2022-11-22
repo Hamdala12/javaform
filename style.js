88258946636bb916c88938aeaf3d0e7cafade3f5
Ahmad B <ahmadbello2011@gmail.com>
2022-11-21 22:33:26 -0800

    javaform

--- style.js
+++ style.js
@@ -8,5 +8,22 @@ function grade() {
         else if (score.value >= 60 && score.value <= 69){
             document.getElementById('output').innerHTML= "B"
         }
+
+        else if (score.value >= 50 && score.value <= 59){
+            document.getElementById('output').innerHTML= "C"
+        }
+
+        else if (score.value >= 45 && score.value <= 49){
+            document.getElementById('output').innerHTML= "D"
+        }
+        else if (score.value >= 40 && score.value <= 45){
+            document.getElementById('output').innerHTML= "E"
+        }
+        else if (score.value >= 0 && score.value <= 40){
+            document.getElementById('output').innerHTML= "F"
+        }
+        else {
+            document.getElementById('output').innerHTML= "Invalid"
+        }
 }
 
